7bb2d73369f3c0bbe4705a77acd04c33158ef09a
ammantza <adonis.mantzaridis@gmail.com>
2025-11-11 13:48:57 +0200

    Week1 Day1: Linux basics + log_parser script

--- demo_folder/log_parser.py
+++ demo_folder/log_parser.py
@@ -3,21 +3,19 @@
 import sys
 
 def parse_log(path):
-
     total = 0
     errors = 0
 
     with open(path, 'r', encoding='utf-8') as f:
         for line in f:
-            total +1
+            total += 1
             if 'ERROR' in line:
                 errors += 1
-
     print(f"Total lines: {total}")
-    print(f"Line with ERROR: {errors}")
+    print(f"Lines with ERROR: {errors}")
 
-    if __name__ == "__main__":
-        if len(sys.argv) != 2:
-            print("Usage: python3 log_parser.py <logfile>")
-        else:
-            parse_log(sys.argv[1])
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 log_parser.py <logfile>")
+    else:
+        parse_log(sys.argv[1])
